cc579282ba0c5708486408999e667c34b599aecb
Brice G <bricegoudalo@gmail.com>
2023-06-20 13:36:54 +0200

    + composer, Router and RouteNotFoundException

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+use Router\Router;
+
+
+require "../vendor/autoload.php";
+
+$router = new Router();
+
+$router->register('/', function () {
+    return 'HomePage';
+});
+
+$router->register('/contact', function () {
+    return 'Contact Page';
+});
+
+echo '<pre>';
+// var_dump($router);
+// Meme si tu tapes "/con" si ça n'existe pas, tu seras redirigé vers "/"
+var_dump($_SERVER['REQUEST_URI']);
+
+// Explode permet de se débarasser des trucs
+// var_dump(explode('?', $_SERVER['REQUEST_URI']));
+
+echo '<pre/>';
+
+
+echo $router->resolve($_SERVER['REQUEST_URI']);
